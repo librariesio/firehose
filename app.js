ae17a50c85cc059714302e499892b5125460dbad
Jeremy Katz <jeremy@tidelift.com>
2018-07-25 15:24:18 -0400

    Add simple / endpoint that will work for a healthcheck

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.get('/', function (req, res) { res.send("OK") })
 app.post('/events', router.firehose.rx);
 app.get('/events', cors(), router.firehose.tx);
 
